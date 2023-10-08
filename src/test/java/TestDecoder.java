8f9b07e45afe9289c0c6007ee87626f905c4bc54
x1stanzz <vova28032003@gmail.com>
2023-10-08 21:19:48 +0300

    Made some test cases a bit more complex

--- src/test/java/TestDecoder.java
+++ src/test/java/TestDecoder.java
@@ -5,24 +5,24 @@ public class TestDecoder {
     @Test
     public void testVowelReplacement(){
         String encodedMessage1 = "M4nk2y";
-        String encodedMessage2 = "1m1t4r";
+        String encodedMessage2 = "1m1t25r, v3d24: cr21t3v3ty";
 
         String decodedMessage1 = Decoder.vowelReplacement(encodedMessage1);
         String decodedMessage2 = Decoder.vowelReplacement(encodedMessage2);
 
         Assertions.assertEquals("Monkey", decodedMessage1);
-        Assertions.assertEquals("amator", decodedMessage2);
+        Assertions.assertEquals("amateur, video: creativity", decodedMessage2);
     }
     @Test
     public void testConsonantReplacement(){
         String encodedMessage1 = "sbdjtn";
-        String encodedMessage2 = "fyqfsjfodf";
+        String encodedMessage2 = "fyqfsjfodf, Qsphsbnnjoh";
 
         String decodedMessage1 = Decoder.consonantReplacement(encodedMessage1);
         String decodedMessage2 = Decoder.consonantReplacement(encodedMessage2);
 
         Assertions.assertEquals("racism", decodedMessage1);
-        Assertions.assertEquals("experience", decodedMessage2);
+        Assertions.assertEquals("experience, Programming", decodedMessage2);
     }
 
     @Test
@@ -31,8 +31,8 @@ public class TestDecoder {
         String decodedMessage1 = Decoder.decodeMessage(encodedMessage1);
         Assertions.assertEquals("universe", decodedMessage1);
 
-        String encodedMessage2 = "Bnbajoh";
+        String encodedMessage2 = "Xibu jt ju?";
         String decodedMessage2 = Decoder.decodeMessage(encodedMessage2);
-        Assertions.assertEquals("Amazing", decodedMessage2);
+        Assertions.assertEquals("What is it?", decodedMessage2);
     }
 }
